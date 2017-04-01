80d9b030aa4699239e093feb50de7dabd767ab44
LTA-Thinking <rbrucej@gmail.com>
2017-04-01 14:04:00 -0400

    Heatmap Database Functions
    Basic database functions. Doesn't access database yet.

--- src/main/java/Heatmap.java
+++ src/main/java/Heatmap.java
@@ -6,15 +6,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
+import org.json.simple.JSONObject;
+
+import com.ibm.bluemix.hack.util.VcapServicesHelper;
+import java.sql.Statement;
+import java.sql.Timestamp;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Font;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.sql.Connection;
 
 @WebServlet("/heatmap")
 public class Heatmap extends HttpServlet {
+	
+	static private DataSource _datasource = null;
 	
 	private Font font = new Font("Heatmap-Font",Font.BOLD,24);
 	/**
@@ -44,6 +59,93 @@ public class Heatmap extends HttpServlet {
         //***********************************************************************************
         
         ImageIO.write(image, "jpeg", response.getOutputStream());
+	}
+	
+	public LinkedList<Location> getLocations()
+	{
+		String databaseURL = "";
+		String dbName = "";
+		
+		LinkedList<Location> locs = new LinkedList<Location>();
+		
+		Connection database = null;
+		Statement stmt;
+		String query = "select LOC_NAME, XCORD, YCORD from " + dbName + ".LOCATIONS";
+		
+		try 
+		{
+			database = DriverManager.getConnection(databaseURL);
+			stmt = database.createStatement();
+			ResultSet results = stmt.executeQuery(query);
+			while(results.next())
+			{
+				int lat = results.getInt("XCORD");
+				int lon = results.getInt("YCORD");
+				String name = results.getString("LOC_NAME");
+				
+				Location loc = new Location(name,lat,lon,getColor(dbName,stmt,name));
+				
+				locs.add(loc);
+			}
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		finally 
+		{
+			try 
+			{
+				if( database != null ) database.close();
+			} 
+			catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		return locs;
+	}
+	
+	public Color getColor(String dbName,Statement stmt,String locName)
+	{
+		double ave = 0;
+		int numScores = 0;
+		Timestamp expireTime = new Timestamp(System.currentTimeMillis()-(int)(1.5*60*60*1000));
+		
+		try
+		{
+			String query = "select SCORE, TIME from " + dbName + " d WHERE d.LOC_NAME = " + locName;
+			ResultSet results = stmt.executeQuery(query);
+			while(results.next())
+			{
+				int score = results.getInt("SCORE");
+				Timestamp created = results.getTimestamp("TIME");
+				
+				if(created.after(expireTime))
+				{
+					ave += score;
+					numScores++;
+				}	
+			}
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		} 
+		
+		Color color;
+		if(ave>0)
+		{
+			ave = ave/numScores-1;
+			color = new Color((int)(255-(5-ave)*51), (int)(255-ave*51), 0);
+		}
+		else
+		{
+			color = Color.gray;
+		}
+		
+		return color;			
 	}
 	
 	public void drawLocation(BufferedImage image, Location loc)
@@ -77,7 +179,22 @@ public class Heatmap extends HttpServlet {
 		g.fillRect(xCord, yCord, width, width);
 	}
 	
-	
+	static public synchronized DataSource getDataSource(){
+		
+		if( _datasource == null ) {
+			JSONObject creds = VcapServicesHelper.getCredentials("compose-for-mysql", null);
+			String connectionString = "jdbc:" + creds.get("uri").toString();
+	        
+	        PoolProperties p = new PoolProperties();
+	        p.setUrl(connectionString);
+	        p.setDriverClassName( "com.mysql.cj.jdbc.Driver" );
+
+	        _datasource = new org.apache.tomcat.jdbc.pool.DataSource( p );
+	        _datasource.setPoolProperties(p);
+		} 
+        
+        return _datasource;
+	}
 
 	
 }
