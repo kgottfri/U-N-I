532adc9712bd834b0a1d8fce34653cc5f151a98f
Adam T. Barson <adam.barson@uvm.edu>
2017-04-01 14:16:12 -0400

    Distance checker in applet

--- src/main/java/Geoloc.java
+++ src/main/java/Geoloc.java
@@ -18,6 +18,7 @@ public class Geoloc extends HttpServlet {
 						pass = "zhUFsTWI29uy",
 						db_url = "asdf";
 	
+	public static final double ACCEPTABLE_DISTANCE = 0.05;
 	// lat & long
 	public static final double[] DAVIS_CENTER = {44.4757914,-73.1964633};
 	public static final double[] MUDDY_WATERS = {44.4761728,-73.2119901};
@@ -25,6 +26,9 @@ public class Geoloc extends HttpServlet {
 	public static final double[] BAILEY_HOWE = {44.4772649,-73.1967532};
 	public static final double[] WATERMAN = {44.478283,-73.201157};
 	public static final double RADIUS = 0.000425383239;
+	
+	private double userLat;
+	private double userLong;
 	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
@@ -37,12 +41,29 @@ public class Geoloc extends HttpServlet {
         response.setContentType("text/html");
         response.getWriter().print("Lat = " + request.getParameter("latitude"));
         response.getWriter().print("Long = " + request.getParameter("longitude"));
+        
+        userLat = Double.parseDouble(request.getParameter("latitude"));
+        userLong = Double.parseDouble(request.getParameter("longitude"));
+        if (inRange(WATERMAN, response)){
+        	response.getWriter().print("You're at waterman!   ");
+        }
+        else{
+        	response.getWriter().print("You're not at waterman!   ");
+        }
         File file = new File("test.txt");
         FileWriter writer = new FileWriter(file);
         PrintWriter w = new PrintWriter(writer);
         
         w.write("hey");
         
+	}
+	
+	public boolean inRange(double [] location, HttpServletResponse response) throws IOException{
+		double lat_dif = location[0] - userLat;
+		double long_dif = location[1] - userLong;
+		double distance = Math.sqrt(Math.pow(lat_dif, 2) + (Math.pow(long_dif, 2)));
+		response.getWriter().print("Distance: " + distance + "   ");
+		return (distance < RADIUS);
 	}
 
 	
