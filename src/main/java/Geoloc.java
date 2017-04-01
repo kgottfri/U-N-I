b9df18949c5e5db398ecfefdbbeda338a6508154
Adam T. Barson <adam.barson@uvm.edu>
2017-04-01 14:20:50 -0400

    javafx removal

--- src/main/java/Geoloc.java
+++ src/main/java/Geoloc.java
@@ -3,8 +3,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javafx.geometry.Point2D;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
