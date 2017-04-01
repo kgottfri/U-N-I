6108f3fc1114fa8cbdb37da44be0d6c5d55014c0
Liv Jensen <Liv.jensen@uvm.edu>
2017-04-01 15:29:27 -0400

    fix in geo loc

--- src/main/java/Geoloc.java
+++ src/main/java/Geoloc.java
@@ -60,8 +60,9 @@ public class Geoloc extends HttpServlet {
         userLat = new BigDecimal(userLat_);
         boolean[] hits = checkLocation(response);
         for (int i = 0; i < hits.length; i++){
-        		if (hits[i])
+        		if (hits[i]){
         			response.getWriter().print(LOCATION_NAMES[i]);
+        		}
         	
         }
         
