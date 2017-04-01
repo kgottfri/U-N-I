51b5ae1c2c0306299b041481de3f36f8caa1bc65
Kevin.Gottfried <Kevin.Gottfried@uvm.edu>
2017-04-01 14:55:59 -0400

    fix to form

--- src/main/webapp/geolocation.js
+++ src/main/webapp/geolocation.js
@@ -14,3 +14,11 @@ function showPosition(position) {
     x.innerHTML = "Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude; 
 }
+//for form page
+function setLocation(){
+	 getLocation();
+	 if(position.coords.longitude == -73.201157 && position.coords.latitude == 44.478283){
+		 var element = document.getElementById("location")
+		 element.value = "Waterman"
+	 }
+}
